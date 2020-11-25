22901a417e8dba647dd9515fb1396a7e6c39e234
meoflauta <meoflauta@gmail.com>
2020-11-25 22:14:41 +0800

    Initial Commit

---
+++ buildnetflixclone/buildnetflixcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  buildnetflixcloneApp.swift
+//  buildnetflixclone
+//
+//  Created by Romeo Flauta on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct buildnetflixcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
