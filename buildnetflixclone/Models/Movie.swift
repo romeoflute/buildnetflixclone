8e24e75972fa92097a51eca13a9270230fd8d27e
meoflauta <meoflauta@gmail.com>
2020-11-25 22:41:50 +0800

    Add Movie model

---
+++ buildnetflixclone/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  buildnetflixclone
+//
+//  Created by Romeo Flauta on 11/25/20.
+//
+
+import Foundation
+
+struct Movie {
+    var id:String
+    var name:String
+    var thumbnailURL:URL
+}
