3ec37d0b70682d67cf89c0f218d3fcc1d085aa10
meoflauta <meoflauta@gmail.com>
2020-11-26 15:47:06 +0800

    Connect submission of risk value to API

---
+++ buildnetflixclone/Utils/GlobalHelpers.swift
@@ -0,0 +1,10 @@
+//
+//  GlobalHelpers.swift
+//  buildnetflixclone
+//
+//  Created by Romeo Flauta on 11/25/20.
+//
+
+import Foundation
+
+let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!)
---
+++ buildnetflixclone/ViewModels/HomeVM.swift
@@ -0,0 +1,12 @@
+//
+//  HomeVM.swift
+//  buildnetflixclone
+//
+//  Created by Romeo Flauta on 11/25/20.
+//
+
+import Foundation
+
+class HomeVM {
+    
+}
