f7c6fdcf17077dc3c11cbf98a5b9a2ee844efa6c
meoflauta <meoflauta@gmail.com>
2020-11-27 15:17:50 +0800

    Show category movies using MVVM

--- buildnetflixclone/ViewModels/HomeVM.swift
+++ buildnetflixclone/ViewModels/HomeVM.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
-class HomeVM {
+class HomeVM: ObservableObject {
+    //String == Category
+    @Published var movies: [String:[Movie]] = [:]
     
+    public var allCategories: [String] {
+        movies.keys.map{String($0)}
+    }
+    
+    func getMovies(forCategory cat: String) -> [Movie]{
+        return movies[cat] ?? []
+    }
+    
+    init(){
+        setupMovies()
+    }
+    
+    func setupMovies(){
+        movies["Trending Now"] = exampleMovies
+        movies["Stand-up Commedy"] = exampleMovies.shuffled()
+        movies["New Releases"] = exampleMovies.shuffled()
+        movies["Watch It Again"] = exampleMovies.shuffled()
+        movies["Sci-fi"] = exampleMovies.shuffled()
+    }
 }
--- buildnetflixclone/Views/Home/HomeView.swift
+++ buildnetflixclone/Views/Home/HomeView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    var vm:HomeVM = HomeVM()
+    
     var body: some View {
         ZStack {
             Color.black
                 .edgesIgnoringSafeArea(.all)
+            
+            ScrollView(showsIndicators:false) {
+                //main vstack
+                LazyVStack {
+                    ForEach(vm.allCategories, id:\.self ){ category in
+                        VStack {
+                            HStack {
+                                Text(category)
+                                    .font(.title3)
+                                    .bold()
+                                Spacer()
+                            }
+                            ScrollView(.horizontal, showsIndicators: false){
+                                LazyHStack{
+                                    ForEach(vm.getMovies(forCategory: category)){movie in
+                                        StandardHomeView(movie: movie)
+                                            .frame(width:100, height:200)
+                                            .padding(.horizontal, 20)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+        .foregroundColor(.white)
     }
 }
 
