ad567658cae4f5c7df63e76c9eacf20763db3e45
meoflauta <meoflauta@gmail.com>
2020-12-01 18:56:43 +0800

    Add search functionality

--- buildnetflixclone/ViewModels/SearchVM.swift
+++ buildnetflixclone/ViewModels/SearchVM.swift
@@ -10,4 +10,55 @@ import SwiftUI
 
 class SearchVM: ObservableObject {
     @Published var isLoading: Bool = false
+    @Published var viewState: ViewState = ViewState.ready
+    @Published var popularMovies: [Movie] = []
+    @Published var searchResults: [Movie] = []
+    
+    @Published var isShowingPopularMovies: Bool = true
+    
+    init(){
+        getPopularMovies()
+    }
+    
+    public func updateSearchText(with text:String){
+        setViewState(to: .loading)
+        if text.count > 0 {
+            isShowingPopularMovies = false
+            getSearchResults(forText: text)
+        }else{
+            isShowingPopularMovies = true
+        }
+    }
+    
+    private func getPopularMovies(){
+        self.popularMovies = generateMovies(40)
+    }
+    
+    private func getSearchResults(forText text:String){
+        let haveResults = Int.random(in: 0...3)
+        DispatchQueue.main.asyncAfter(deadline:.now() + 2){
+            if haveResults == 0 {//empty
+                self.searchResults = []
+                self.setViewState(to: .empty)
+            }else{
+                let movies = generateMovies(21)
+                self.searchResults = movies
+                self.setViewState(to: .ready)
+            }
+        }
+    }
+    
+    private func setViewState(to state: ViewState){
+        DispatchQueue.main.async {
+            self.viewState = state
+            self.isLoading = state == .loading
+        }
+    }
+}
+
+enum ViewState {
+    case empty
+    case loading
+    case ready
+    case error
 }
